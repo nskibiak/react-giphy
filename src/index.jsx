094815c83feaa0b23008a2fb65f3deac00465004
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-03-19 12:55:43 +0900

    created App class

--- src/index.jsx
+++ src/index.jsx
@@ -3,40 +3,7 @@ import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-// const Hello = ({ name }) => {
-//   return (
-//     <div>
-//       Hello, {name}
-//     </div>
-//   );
-// };
 
-class Hello extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      clicked: false
-    };
-  }
-
-  handleClick = () => {
-    // TODO: change the state!
-    this.setState({
-      clicked: !this.state.clicked
-    });
-  }
-
-  render() {
-    // BUILD AND RETURN HTML
-    return (
-      <div className={this.state.clicked ? 'clicked' : null }
-        onClick={this.handleClick}>
-        Hello {this.props.name}
-      </div>
-    )
-  }
-}
 
 const root = document.getElementById('root');
 if (root) {
