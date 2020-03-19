12ac02cbd3a2975a21a66d313abd6abf9d06b4ca
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-03-19 14:48:24 +0900

    added searchbar component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,19 +2,34 @@ import React, { Component } from 'react';
 
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
-import GifList from '.gif-list.jsx';
+import GifList from './gif_list.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: 'd2ZfqZY5eSCR0rza'
+    }
+  }
   render() {
+    const gifs = [
+      {id: 'd2ZfqZY5eSCR0rza'},
+      {id: 'xT9IgDEI1iZyb2wqo8'}
+    ]
+
     return (
       <div>
         <div className="left-scene">
           <SearchBar />
           <div className="selected-gif">
-            <Gif id='d2ZfqZY5eSCR0rza'/>
+            <Gif id={this.state.selectedGifId}/>
           </div>
         </div>
-        <div className="right-scene"></div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs}/>
+        </div>
       </div>
     )
   }
--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  handleChange = (event) => {
+    console.log(event.target.value);
+  }
   render() {
     return (
-      <input type="text" className="form-search form-control"/>
+      <input type="text" className="form-search form-control"
+        onChange={this.handleChange}/>
     )
   }
 }
