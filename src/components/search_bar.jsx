ecbf9ea849d179c18ec37b3a0793db6288cb18d4
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-03-22 13:10:45 +0900

    refactored SearchBar class

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+
+  }
+
   handleChange = (event) => {
     this.props.searchFunction(event.target.value);
   }
+
   render() {
     return (
-      <input type="text" className="form-search form-control"
-        onChange={this.handleChange}/>
-    )
+      <input
+        type="text"
+        className="form-search form-control"
+        onChange={this.handleChange}
+      />
+    );
   }
+
 }
 
 export default SearchBar;
